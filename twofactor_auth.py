79e241521e18de6d66fdcf6a9009978525379067
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-09 08:48:43 +0000

    implemented time based otp

--- twofactor_auth.py
+++ twofactor_auth.py
@@ -1,4 +1,14 @@
 import time
 import pyotp
 
-key = pyotp.random_base32()
+key = 'letsdothis'
+# key = pyotp.random_base32()
+
+print(key)
+
+totp = pyotp.TOTP(key)
+print(totp.now())
+
+input_code = input('Enter 2FA Code: ')
+
+print(totp.verify(input_code))
