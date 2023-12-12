df78a83f89d1f97d0b980a3e136608b7c1166382
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-12 19:05:03 +0000

    Implemented logger function

--- first_class_functions.py
+++ first_class_functions.py
@@ -7,32 +7,45 @@ First-Class Citizens (Programming):
 
 '''
 
-def square(x):
-    return x * x
+# def square(x):
+#     return x * x
 
-f = square(5)
+# f = square(5)
 
-print(square)
-print(f)
+# print(square)
+# print(f)
 
 
-# Assigning the function to a variable
-g = square
+# # Assigning the function to a variable
+# g = square
 
-print(g)
-print(g(5))
+# print(g)
+# print(g(5))
 
 '''
 A higher order function accepts a function as an argument or returns a function as a result#
 
 '''
 
-# A map function
-def my_map(func, arg_list):
-    result = []
-    for item in arg_list:
-        result.append(func(item))
-    return result
+# # A map function that accepts a function as an argument
+# def my_map(func, arg_list):
+#     result = []
+#     for item in arg_list:
+#         result.append(func(item))
+#     return result
 
-squares = my_map(square, [1,2,3,4,5])
-print(squares)
+# squares = my_map(square, [1,2,3,4,5])
+# print(squares)
+
+# A function that returns a function
+
+def logger(msg):
+
+    def log_message():
+        print('log:', msg)
+    
+    return log_message
+
+
+log_hi = logger('Hi')
+log_hi()
