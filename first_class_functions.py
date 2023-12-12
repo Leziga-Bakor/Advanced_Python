db2821d5e26a0095e6a4af0c76bc1293964ed4f9
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-12 19:43:11 +0000

    Added html_tag for returning function

--- first_class_functions.py
+++ first_class_functions.py
@@ -39,13 +39,26 @@ A higher order function accepts a function as an argument or returns a function 
 
 # A function that returns a function
 
-def logger(msg):
+# def logger(msg):
 
-    def log_message():
-        print('log:', msg)
+#     def log_message():
+#         print('log:', msg)
     
-    return log_message
+#     return log_message
 
 
-log_hi = logger('Hi')
-log_hi()
+# log_hi = logger('Hi')
+# log_hi()
+
+def html_tag(tag):
+
+    def wrap_text(msg):
+        print('<{0}>{1}</{0}>'.format(tag,msg))
+    return wrap_text
+
+print_h1 = html_tag('h1')
+print_h1('Test Headline!')
+print_h1('Another Headline')
+
+print_p = html_tag('p')
+print_p('Test Paragraph!')
