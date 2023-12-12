59b5eb5a0484ff3d7a6b2a2f35083769bc090972
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-12 18:48:06 +0000

    Added code to assign function to a variable

--- first_class_functions.py
+++ first_class_functions.py
@@ -4,4 +4,20 @@ First-Class Functions:
 
 First-Class Citizens (Programming):
 "A first-class citizen (sometimes called first class objects) in a programming language is an entity which supports all the operations generally available to other entities. these operations typically include being passed as an argument, returned from a function and assigned to a variable"
+
 '''
+
+def square(x):
+    return x + x
+
+f = square(5)
+
+print(square)
+print(f)
+
+
+# Assigning the function to a variable
+g = square
+
+print(g)
+print(g(5))
