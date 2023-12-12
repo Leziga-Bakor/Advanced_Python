ffa97a3208659b50222e60fb37592df45051310c
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-12 18:41:47 +0000

    Added definition for first class function

---
+++ first_class_functions.py
@@ -0,0 +1,7 @@
+'''
+First-Class Functions:
+"A Programming Languate is said to have first-class functions if it treats functions as first-class citizens"
+
+First-Class Citizens (Programming):
+"A first-class citizen (sometimes called first class objects) in a programming language is an entity which supports all the operations generally available to other entities. these operations typically include being passed as an argument, returned from a function and assigned to a variable"
+'''
