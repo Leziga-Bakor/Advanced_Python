1bf890d15d5878b063b1b41e2c6a22ad41bb4c11
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-13 00:21:12 +0000

    Modified outer_func to accept msg argument

--- closures.py
+++ closures.py
@@ -2,13 +2,13 @@
 A closure is a record storing a function together with an environment: a mapping associating each free variable of a function with the value or storage location to which the name was bound when the closure was created. A closure unlike a plain function allows the function to access those captured variables through the closure's reference to them, even when the function is invoked outside their scope.
 '''
 
-def outer_func():
-    message = 'Hi'
+def outer_func(msg):
+    message = msg
     def inner_func():
         print(message)
     return inner_func
 
-my_func = outer_func()
-
+my_func = outer_func('Hi')
+my_func2 = outer_func('Hello')
 my_func()
 my_func()
