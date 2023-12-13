652ee4f50bea4d038276af0b440ded85a109f8cb
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-13 00:13:41 +0000

    Added definition for closure

---
+++ closures.py
@@ -0,0 +1,3 @@
+'''
+A closure is a record storing a function together with an environment: a mapping associating each free variable of a function with the value or storage location to which the name was bound when the closure was created. A closure unlike a plain function allows the function to access those captured variables through the closure's reference to them, even when the function is invoked outside their scope.
+'''
