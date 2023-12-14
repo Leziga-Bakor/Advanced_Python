895781adf32d9e3229c1d909d3ac15666a24e126
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-14 12:50:52 +0000

    Replaced outter and innter functions with decorator and wrapper functions

--- decorators.py
+++ decorators.py
@@ -1,8 +1,12 @@
 def outer_function(msg):
-    message = msg
     def inner_function():
-        print(message)
+        print(msg)
     return inner_function
+
+def decorator_function(message):
+    def wrapper_function()
+        print(message)
+    return wr 
 
 hi_func = outer_function('Hi')
 bye_func = outer_function('Bye')
@@ -10,4 +14,10 @@ bye_func = outer_function('Bye')
 hi_func()
 bye_func()
 
-    
+'''
+What is a Decorator?
+
+A decorator is a function that takes another function as an argument. It can perform actions around the function argument without changing structure of original function
+
+'''
+
