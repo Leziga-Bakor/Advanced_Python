37bf25232a6df95931cef75364174823c256273f
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-14 20:43:37 +0000

    Implemented decorator class

--- decorators.py
+++ decorators.py
@@ -4,6 +4,14 @@ def decorator_function(original_function):
         return original_function(*args, **kwargs)
     return wrapper_function
 
+class decorator_class(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print(f' Call method executed this before {self.original_function.__name__}')
+        return self.original_function(*args, **kwargs)
+        
 # def display():
 #     print('display function ran')
 
@@ -22,7 +30,7 @@ A decorator is a function that takes another function as an argument. It can per
 
 # display()
 
-@decorator_function
+@decorator_class
 def display_info(name, age):
     print('display_info ran with argments {}. {}'.format(name,age))
 
