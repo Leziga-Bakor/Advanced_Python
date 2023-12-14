ce3d55159764628177d36fba47a5b84ed5e5ea0e
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-14 13:38:35 +0000

    Improved wrapper function to accepts *args and *kwargs

--- decorators.py
+++ decorators.py
@@ -1,7 +1,7 @@
 def decorator_function(original_function):
-    def wrapper_function():
+    def wrapper_function(*args, **kwargs):
         print(f'wrapper executed this before {original_function.__name__}')
-        return original_function()
+        return original_function(*args, **kwargs)
     return wrapper_function
 
 # def display():
@@ -16,8 +16,14 @@ What is a Decorator?
 A decorator is a function that takes another function as an argument. It can perform actions around the function argument without changing structure of original function
 
 '''
-@decorator_function
-def display():
-    print('display function ran')
+# @decorator_function
+# def display():
+#     print('display function ran')
 
-display()
+# display()
+
+@decorator_function
+def display_info(name, age):
+    print('display_info ran with argments {}. {}'.format(name,age))
+
+display_info('john',25)
