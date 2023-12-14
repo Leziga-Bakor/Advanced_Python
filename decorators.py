3b971329ab209bece14dc869e0bf5c5ce9e52eec
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-14 12:58:38 +0000

    Updated decorator function

--- decorators.py
+++ decorators.py
@@ -1,5 +1,6 @@
 def decorator_function(original_function):
     def wrapper_function():
+        print(f'wrapper executed this before {original_function.__name__}')
         return original_function()
     return wrapper_function
 
