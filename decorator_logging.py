ff98803dc7423625a0111d2a526187922715297c
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-15 22:25:52 +0000

    Implemented my_timer function

--- decorator_logging.py
+++ decorator_logging.py
@@ -9,3 +9,10 @@ def my_logger(orig_func):
         return orig_func(*args, **kwargs)
     return wrapper
 
+def my_timer(orig_func):
+    import time
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in {} sec'.format(orig_func.__name__, t2))
