95ebdb1933df45e6f02f0046114be421aa3c2a52
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-16 23:03:17 +0000

    Added argument for verbosity

--- argparse_try.py
+++ argparse_try.py
@@ -6,11 +6,24 @@ parser.add_argument('greeting', help = 'The greeting message display')
 
 parser.add_argument('-n', '--numbers', type=float, nargs='*', help = 'The Numbers to be displayed')
 
+parser.add_argument('-v', '--verbosity', type=int, choices=[0,1,2], help = 'Determines how much information is displayed')
+
 args = parser.parse_args()
 
 print(args)
 print(args.greeting)
 print(args.numbers)
 
-if args.numbers is not None:
-    print(sum(args.numbers))
+if args.verbosity is None:
+    print(args.greeting)
+    if args.numbers is not None:
+        print(sum(args.numbers))
+else:
+    if args.verbosity >= 0:
+        print(args.greeting)
+        if args.numbers is not None:
+            print(sum(args.numbers))
+    if args.verbosity >= 1:
+        print(args.numbers)
+    if args.verbosity == 2:
+        print('Extra information')
