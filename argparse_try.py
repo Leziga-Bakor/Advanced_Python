748cc0ca4eb51095497a6852fd3f0fb44813c038
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-16 22:46:53 +0000

    Improved number argument to take any amount of numbers

--- argparse_try.py
+++ argparse_try.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('greeting', help = 'The greeting message display')
 
-parser.add_argument('-n', '--numbers', type=float, nargs=2, help = 'The Numbers to be displayed')
+parser.add_argument('-n', '--numbers', type=float, nargs='*', help = 'The Numbers to be displayed')
 
 args = parser.parse_args()
 
@@ -13,4 +13,4 @@ print(args.greeting)
 print(args.numbers)
 
 if args.numbers is not None:
-    print(args.numbers[0]+args.numbers[1])
+    print(sum(args.numbers))
