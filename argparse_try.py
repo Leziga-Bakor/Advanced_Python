0aeb05820b6986049912d9e7d265b72c5411c771
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-16 22:44:10 +0000

    Added number argument to accept two numbers and add them

--- argparse_try.py
+++ argparse_try.py
@@ -4,7 +4,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('greeting', help = 'The greeting message display')
 
+parser.add_argument('-n', '--numbers', type=float, nargs=2, help = 'The Numbers to be displayed')
+
 args = parser.parse_args()
 
 print(args)
 print(args.greeting)
+print(args.numbers)
+
+if args.numbers is not None:
+    print(args.numbers[0]+args.numbers[1])
