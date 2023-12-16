21327a70551e612206f3b6571066c15d9fcb95d2
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-16 23:11:25 +0000

    Added debug argument

--- argparse_try.py
+++ argparse_try.py
@@ -1,14 +1,18 @@
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('greeting', help = 'The greeting message display')
-
 parser.add_argument('-n', '--numbers', type=float, nargs='*', help = 'The Numbers to be displayed')
-
 parser.add_argument('-v', '--verbosity', type=int, choices=[0,1,2], help = 'Determines how much information is displayed')
+# parser.add_argument('-f', '--file', type=str)
+parser.add_argument('--debug', action='store_true', help='Enables debug mode')
 
 args = parser.parse_args()
+
+if args.debug:
+    start = time.perf_counter()
 
 print(args)
 print(args.greeting)
@@ -27,3 +31,7 @@ else:
         print(args.numbers)
     if args.verbosity == 2:
         print('Extra information')
+
+if args.debug:
+    end = time.perf_counter()
+    print(end -start)
