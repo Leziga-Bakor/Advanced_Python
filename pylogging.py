48a156917159da1a664488ff84659731a6b698e6
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-18 20:47:20 +0000

    Included discriptions for logging levels

--- pylogging.py
+++ pylogging.py
@@ -1,3 +1,16 @@
+
+"""
+Logging Levels
+
+1. Debug: Detailed information, typically of interest only when diagnosing problems.
+2. INFO: Confirmation that things are working as expected.
+3. WARNING: An indication that somethin unexpected happened, or indicative of some problem in the near future (e.g. 'disk space low'). The software is still working as expected.
+4. ERROR: Due to a more serious problem, the software has not been able to perform some function.
+5. CRITICAL: A serious error, indicating that the program itself may be unalbe to continue running.
+
+"""
+
+
 def add(x,y):
     """ Add Function """
     return x + y
