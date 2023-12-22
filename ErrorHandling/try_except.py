65720635382304b518ae96ab0ef4baf5d67fec28
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-22 22:22:54 +0000

    Added comment for finally

--- ErrorHandling/try_except.py
+++ ErrorHandling/try_except.py
@@ -2,10 +2,10 @@ try:
     f = open('test_file.txt')
 except Exception:
     print(('Sorry. file does not exist'))
-# else:
-#     print('this session handled what code does if try block doesnt catch error)
-# finally:
-#     pass
+else:
+    print('this session handled what code does if try block doesnt catch error')
+finally:
+    print('This runs no regardless of what happens in the  try block')
     
 '''
 More general exception should be last
