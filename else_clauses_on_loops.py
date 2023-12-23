5f9a48a124315cf75ef27e89b0bca8b531533751
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-23 20:41:48 +0000

    Added while loop with else statement

--- else_clauses_on_loops.py
+++ else_clauses_on_loops.py
@@ -1,6 +1,19 @@
+
+'''
 my_list = [1,2,3,4,5]
 
 for i in my_list:
     print(i)
+    if i == 3:
+        break
 else:
     print('Hit the for/else statement')
+
+'''
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+else:
+    print('Hit the while/Else statement')
