1334e2f5f4209161f5bd2ab138495dd03ed3f962
Leziga Bakor <85461869+Leziga-Bakor@users.noreply.github.com>
2023-12-23 21:49:10 +0000

    Added example with else block. find_index function

--- else_clauses_on_loops.py
+++ else_clauses_on_loops.py
@@ -24,3 +24,15 @@ else:
 '''
 else after a loop can be seen as no break. It executes only when the loop dosent meet a break statement
 '''
+def find_index(to_search, target):
+    for i, value in enumerate(to_search):
+        if value == target:
+            break
+    else:
+        return -1
+    return i
+
+my_list = ['Jack', 'John', 'Wick']
+index_location = find_index(my_list, 'John')
+
+print('Location of target is index: {}'.format(index_location))
