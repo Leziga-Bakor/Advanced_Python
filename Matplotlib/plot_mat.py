71dfadcfae5da2cdc1cc7189438a90f552b9a2e4
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-25 22:46:19 +0000

    Added colour to points on chart

--- Matplotlib/plot_mat.py
+++ Matplotlib/plot_mat.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 X_data = np.random.random(50) * 100
 Y_data = np.random.random(50) * 100
 
-plt.scatter(X_data, Y_data)
+plt.scatter(X_data, Y_data, c="red")
 plt.show()
