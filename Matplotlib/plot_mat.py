649d692f141f0f6cd2de4be1007fbd25d43edbb0
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-25 22:56:36 +0000

    Added second example for line plot

--- Matplotlib/plot_mat.py
+++ Matplotlib/plot_mat.py
@@ -1,8 +1,18 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+'''
 X_data = np.random.random(50) * 100
 Y_data = np.random.random(50) * 100
 
 plt.scatter(X_data, Y_data, c="red", s=100, marker='*')
+plt.show()
+
+'''
+
+years = [2006 + x for x in range(16)]
+weights = [80,83,84,85,86,82,81,79,83,
+           83,80,82,82,83,81,79]
+
+plt.plot(years,weights)
 plt.show()
