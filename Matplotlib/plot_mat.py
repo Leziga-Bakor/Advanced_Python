087f2fe8a4284800d20567fe84e95d2dccf16141
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-26 09:16:01 +0000

    Added bins to histogram and plotted histogram with cummulative

--- Matplotlib/plot_mat.py
+++ Matplotlib/plot_mat.py
@@ -34,5 +34,8 @@ plt.show()
 
 # Histogram
 ages = np.random.normal(20,1.5,1000)
-plt.hist(ages)
+plt.hist(ages, bins=[ages.min(), 18,21,ages.max()])
+plt.show()
+
+plt.hist(ages, bins=20,cumulative=True)
 plt.show()
