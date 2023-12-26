b666ebb30a68b588bc75e362d139ef7429a4ccee
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-26 09:47:50 +0000

    Added customization options for pie chart

--- Matplotlib/plot_mat.py
+++ Matplotlib/plot_mat.py
@@ -46,6 +46,8 @@ plt.show()
 # pie chart
 langs=['Python','C++','Java','C#','Go']
 votes=[50,24,14,6,17]
+explodes = [0,0,0,0.2,0]
 
-plt.pie(votes,labels=langs)
+plt.pie(votes,labels=langs, explode=explodes,
+        autopct='%.2f%%', pctdistance=1.3, startangle=90)
 plt.show()
