cd7433f2b2db838721208182fd03aa61a226fddd
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-27 13:16:17 +0000

    Added code for scatter plot to the file.

---
+++ Matplotlib/scatterPlot.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X_data = np.random.random(50) * 100
+Y_data = np.random.random(50) * 100
+
+plt.scatter(X_data, Y_data, c="red", s=100, marker='*')
+plt.show()
