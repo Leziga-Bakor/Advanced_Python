23c394ec6964716b3aff20677399143415ccb718
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-28 16:28:26 +0000

    Added data for 3 company and plot

---
+++ Matplotlib/multiline_plots.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+stock_a = [100,102,99,101,101,100,102]
+stock_b = [90,95,102,104,105,103,109]
+stock_c = [110,115,100,105,100,98,95]
+
+plt.plot(stock_a, label='company1')
+plt.plot(stock_b, label='Company2')
+plt.plot(stock_c, label='Company3')
+
+plt.show()
