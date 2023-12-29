1bc26f80285462d54d3c00e693d8f4a9cbda4988
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-29 14:54:22 +0000

    Added code to plot 4 sub plots

---
+++ Matplotlib/multiPlotsInSameView.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.arange(100)
+
+fig, axs = plt.subplots(2,2)
+
+axs[0,0].plot(x, np.sin(x))
+axs[0,0].set_title('Sine Wave')
+
+axs[0,1].plot(x, np.cos(x))
+axs[0,1].set_title('Cosine Wave')
+
+axs[1,0].plot(x, np.random.random(100))
+axs[1,0].set_title('Random Function')
+
+axs[1,1].plot(x, np.tan(x))
+axs[1,1].set_title('Tan Function')
+
+plt.show()
+
