25f49d6f115aa7a42442d6bba58a288d219f82af
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-30 14:51:11 +0000

    Added codes for 3D line plot

---
+++ Matplotlib/3Dline.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+ax = plt.axes(projection="3d")
+
+x = np.arange(0,50,0.1)
+y = np.sin(x)
+z = np.cos(x)
+
+ax.plot(x,y,z)
+ax.set_title("3D Plot")
+ax.set_xlabel('Data')
+ax.set_zlabel('Cos')
+ax.set_ylabel('Sin')
+plt.show()
