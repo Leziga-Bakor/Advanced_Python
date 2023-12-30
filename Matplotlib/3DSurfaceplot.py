352a89e7f213e212e82586e166e134157a020fa2
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-30 15:02:10 +0000

    Added code for surface plot

---
+++ Matplotlib/3DSurfaceplot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+ax = plt.axes(projection="3d")
+
+x = np.arange(-5,5,0.1)
+y = np.arange(-5,5,0.1)
+
+X,Y = np.meshgrid(x,y)
+
+Z = np.sin(X) + np.cos(Y)
+
+
+ax.plot_surface(X,Y,Z)
+ax.set_title("3D Surface Plot")
+
+plt.show()
