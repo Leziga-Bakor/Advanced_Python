2a2eed14f36fb7cc4cff6abc2c657446a836893d
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-01 16:15:32 +0000

    Added numpy array

---
+++ Numpy/introduction.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+# Numpy Array
+a = np.array([1,2,3,4,5])
+
+a_mul = np.array([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]])
+
+print(a_mul.shape)
+print(a_mul.ndim)
+print(a_mul.size)
+print(a_mul.dtype)
