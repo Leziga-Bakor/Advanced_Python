021ae398aabec4fb3c6cfa0199133b91648849ea
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-01 16:33:58 +0000

    Type casting

--- Numpy/introduction.py
+++ Numpy/introduction.py
@@ -1,5 +1,6 @@
 import numpy as np 
 
+'''
 # Numpy Array
 a = np.array([1,2,3,4,5])
 
@@ -11,3 +12,12 @@ print(a_mul.shape)
 print(a_mul.ndim)
 print(a_mul.size)
 print(a_mul.dtype)
+'''
+
+
+# Static typing in numpy
+a = np.array([[1,2,3],
+              [4, 'Hello', 6],
+              [7,8,9]])
+
+print(a.dtype)
