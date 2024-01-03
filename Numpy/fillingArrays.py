7713e384f82c1092f8a620d293f91f6ba3f8d96a
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-03 19:58:07 +0000

    Initialized empty array

--- Numpy/fillingArrays.py
+++ Numpy/fillingArrays.py
@@ -1,10 +1,13 @@
 import numpy as np 
 
 a = np.full((2,3,4), 9) # given the size, creates an array filling with one value'
-print(a)
+# print(a)
 
 a = np.zeros((10,5,2))
-print(a)
+# print(a)
 
 a=np.ones((10,5,2))
+# print(a)
+
+a = np.empty((5,5,5))
 print(a)
