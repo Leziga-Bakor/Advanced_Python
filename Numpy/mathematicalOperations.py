4499186c59c23b2f34919924e3171be5ccad3443
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-11 23:05:17 +0000

    Addition operation in python list vs numpy list

--- Numpy/mathematicalOperations.py
+++ Numpy/mathematicalOperations.py
@@ -6,5 +6,8 @@ l2 = [6,7,8,9,0]#
 a1 = np.array(l1)
 a2 = np.array(l2)
 
-print(l1*5)
-print((a1*5))
+print(l1*5) # repeats list n times
+print(a1*5) # muliplies each item in list by n
+
+print(l1+l1) # This concatenates two lists
+print(a1+5) # Adds 5 to individual list items
