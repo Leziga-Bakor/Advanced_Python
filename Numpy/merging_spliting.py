db8edc261eee4d8834cd8717b5240387e7dbc013
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-13 22:47:45 +0000

    Added code for concatenating

---
+++ Numpy/merging_spliting.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# joining and splitting
+a1=np.array([[1,2,3,4,5],
+             [6,7,8,9,10]])
+a2 =(
+    [
+        [11,12,13,14,15],
+        [16,17,18,19,20]
+    ]
+)
+
+a = np.concatenate((a1,a2))
