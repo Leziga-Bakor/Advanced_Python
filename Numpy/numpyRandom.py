d73a288fff25446fd8b4820ef1bf7d26a48146c9
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-15 23:51:34 +0000

    Save numpy array

--- Numpy/numpyRandom.py
+++ Numpy/numpyRandom.py
@@ -7,3 +7,5 @@ number = np.random.randint(90, 100, size=(2,3,4))
 num = np.random.bionomial(10, p=0.5, size=(5,10))
 
 numbers = np.random.choice([10,20,30,40,50],size=(5,10))
+
+np.save('myarray.npy', a)
