3dea4568d2b0a162c191f37cd26556b3f556cc44
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-15 23:49:53 +0000

    generate random numbers

--- Numpy/numpyRandom.py
+++ Numpy/numpyRandom.py
@@ -5,3 +5,5 @@ number = np.random.randint(100)
 number = np.random.randint(90, 100, size=(2,3,4))
 
 num = np.random.bionomial(10, p=0.5, size=(5,10))
+
+numbers = np.random.choice([10,20,30,40,50],size=(5,10))
