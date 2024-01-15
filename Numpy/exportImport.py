ed5d5fbc9d690c3d9473b0498f19f5944290f0a3
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-15 23:59:35 +0000

    Added code to import array using load

--- Numpy/exportImport.py
+++ Numpy/exportImport.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-b=np.array(
-    [
-        [1,2,3,4,5],
-        [6,7,8,9,10],
-        [11,12,13,14,15],
-        [16,17,18,19,20]
-    ]
-)
+# b=np.array(
+#     [
+#         [1,2,3,4,5],
+#         [6,7,8,9,10],
+#         [11,12,13,14,15],
+#         [16,17,18,19,20]
+#     ]
+# )
 
-np.save('myarray.npy', b)
+# np.save('myarray.npy', b)
+
+a = np.load('myarray.npy')
+print(a)
