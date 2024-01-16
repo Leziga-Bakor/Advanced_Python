bd0fe4f60729560738d7c78da6110607adf74f6a
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-16 22:36:25 +0000

    Added code to load from csv

--- Numpy/exportImport.py
+++ Numpy/exportImport.py
@@ -14,10 +14,12 @@ import numpy as np
 # a = np.load('myarray.npy')
 # print(a)
 
-a = np.array([[1,2,3,4,5,6],
-              [7,8,9,10,11,12],
-              [13,14,15,16,17,18],
-              [18,19,20,21,22,23]
-              ])
+# a = np.array([[1,2,3,4,5,6],
+#               [7,8,9,10,11,12],
+#               [13,14,15,16,17,18],
+#               [18,19,20,21,22,23]
+#               ])
 
-np.savetxt('myarray.csv', a, delimiter = ",")
+# np.savetxt('myarray.csv', a, delimiter = ",")
+a = np.loadtxt('myarray.csv', delimiter=',')
+print(a)
