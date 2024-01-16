9d7e953d25611acd495083d39baecca9b81f027e
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-16 22:27:11 +0000

    Added code for exporting to csv

--- Numpy/exportImport.py
+++ Numpy/exportImport.py
@@ -11,5 +11,13 @@ import numpy as np
 
 # np.save('myarray.npy', b)
 
-a = np.load('myarray.npy')
-print(a)
+# a = np.load('myarray.npy')
+# print(a)
+
+a = np.array([[1,2,3,4,5,6],
+              [7,8,9,10,11,12],
+              [13,14,15,16,17,18],
+              [18,19,20,21,22,23]
+              ])
+
+np.savetxt('myarray.csv', a, delimiter = ",")
